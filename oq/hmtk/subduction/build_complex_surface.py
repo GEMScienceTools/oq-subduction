298af2971b3ac41921fa10e3bf4f2443bc8f5a0c
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-02-03 14:24:27 +0100

    Now the cut of profiles also include interpolation

--- oq/hmtk/subduction/build_complex_surface.py
+++ oq/hmtk/subduction/build_complex_surface.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import re
 import sys
-import glob
-import h5py
 import numpy
-import filecmp
-import logging
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-from openquake.hazardlib.geo.geodetic import distance, point_at, azimuth
+from openquake.baselib import sap
 
 from oq.hmtk.subduction.create_2pt5_model import (read_profiles_csv,
                                                   get_profiles_length,
@@ -21,81 +12,76 @@ from oq.hmtk.subduction.create_2pt5_model import (read_profiles_csv,
                                                   write_profiles_csv)
 
 
-def main(argv):
+def build_complex_surface(in_path, max_sampl_dist, out_path, upper_depth=0,
+                          lower_depth=1000, from_id='.*', to_id='.*'):
     """
-    argv[0] - Folder name
-    argv[1] - Sampling distance [km]
-    argv[2] - Output folder name
-    argv[3] - Upper seismogenic depth [km]
-    argv[4] - Lower seismogenic depth [km]
+    :param str in_path:
+        Folder name
+    :param str float in_path:
+        Sampling distance [km]
+    :param str out_path:
+        Folder name
+    :param float upper_depth:
+        The depth above which we cut the profiles
+    :param float lower_depth:
+        The depth below which we cut the profiles
     """
-    #
-    #
-    in_path = os.path.abspath(argv[0])
-    maximum_sampling_distance = float(argv[1])
-    out_path = os.path.abspath(argv[2])
-    #
-    # set upper depth
-    upper_depth = 0.0
-    if len(argv) > 3:
-        upper_depth = float(argv[3])
-    #
-    # set lower depth
-    lower_depth = 1000.
-    if len(argv) > 4:
-        lower_depth = float(argv[4])
-    #
-    # selection label
-    from_id = ".*"
-    if len(argv) > 5:
-        from_id = argv[5]
-    #
-    # selection label
-    to_id = ".*"
-    if len(argv) > 6:
-        to_id = argv[6]
-    #
-    # Check input
-    if len(argv) < 3:
-        tmps = 'Usage: build_complex_surface.py <in_folder>'
-        tmps += ' <sampling_dist> <out_folder> <lower_seism_depth>'
-        print (tmps)
-        exit(0)
     #
     # Check input and output folders
     if in_path == out_path:
         tmps = '\nError: the input folder cannot be also the output one\n'
-        tmps += '    input: {0:s}\n'.format(in_path)
-        tmps += '    input: {0:s}\n'.format(out_path)
-        print (tmps)
+        tmps += '    input : {0:s}\n'.format(in_path)
+        tmps += '    output: {0:s}\n'.format(out_path)
+        print(tmps)
         exit(0)
     #
     # read profiles
     sps, dmin, dmax = read_profiles_csv(in_path, upper_depth, lower_depth,
                                         from_id, to_id)
-
     #
     # compute length of profiles
     lengths, longest_key, shortest_key = get_profiles_length(sps)
-    print('key:', longest_key, shortest_key)
+    print('Longest profile (id: {:s}): {:2f}'.format(longest_key,
+                                                     lengths[longest_key]))
+    print('Shortest profile (id: {:s}): {:2f}'.format(shortest_key,
+                                                      lengths[shortest_key]))
+    print('Depth min: {:.2f}'.format(dmin))
+    print('Depth max: {:.2f}'.format(dmax))
     #
     #
-    number_of_samples = numpy.ceil(lengths[longest_key] /
-                                   maximum_sampling_distance)
-    print ('Number of subsegments:', number_of_samples)
+    number_of_samples = numpy.ceil(lengths[longest_key] / max_sampl_dist)
+    print('Number of subsegments for each profile:', number_of_samples)
     tmp = lengths[shortest_key]/number_of_samples
-    print ('Shortest sampling [%s]: %.4f' % (shortest_key, tmp))
+    print('Shortest sampling [%s]: %.4f' % (shortest_key, tmp))
     tmp = lengths[longest_key]/number_of_samples
-    print ('Longest sampling  [%s]: %.4f' % (longest_key, tmp))
+    print('Longest sampling  [%s]: %.4f' % (longest_key, tmp))
     #
     # resampled profiles
     rsps = get_interpolated_profiles(sps, lengths, number_of_samples)
     #
     # store new profiles
-    write_profiles_csv(rsps, argv[2])
+    write_profiles_csv(rsps, out_path)
     #
     # store computed edges
-    write_edges_csv(rsps, argv[2])
+    write_edges_csv(rsps, out_path)
+
+
+def main(argv):
+
+    p = sap.Script(build_complex_surface)
+    p.arg(name='in_path', help='Path to the input folder')
+    p.arg(name='max_sampl_dist', help='Maximum profile sampling distance')
+    p.arg(name='out_path', help='Path to the output folder')
+    p.opt(name='upper_depth', help='Upper depth')
+    p.opt(name='lower_depth', help='Lower depth')
+    p.opt(name='from_id', help='Index profile where to start the sampling')
+    p.opt(name='to_id', help='Index profile where to stop the sampling')
+
+    if len(argv) < 1:
+        print(p.help())
+    else:
+        p.callfunc()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
